ea65bec80d0d7d96804b75e9a646421b09315a50
mrappi752@gmail.com <mrappi752@gmail.com>
2024-02-19 11:23:37 +0700

    menambahkan daftar produk

---
+++ main.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+ import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQhFoRf_4ecIp-b7pclOk29ncMXpjzELE",
+  authDomain: "insan-cemerlang-58879.firebaseapp.com",
+  projectId: "insan-cemerlang-58879",
+  storageBucket: "insan-cemerlang-58879.appspot.com",
+  messagingSenderId: "704155633968",
+  appId: "1:704155633968:web:42d7eeff6bb87ae7af60f5",
+  measurementId: "G-PPMEB42GMS"
+};
+
+// Inisialisasi firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarProduk(){
+  const refDokumen = collection(db, "produk");
+  const kuery = query(refDokumen, orderBy("nama"));
+  const cuplikankuery = await getDocs(kuery);
+  
+  let hasil = [];
+  cuplikankuery.forEach((dok) => {
+    hasil.push({ 
+      id: dok.id, 
+      nama: dok.data().nama,
+      harga: dok.data().harga,
+      stok: dok.data().stok,
+    });
+  });
+  
+  return hasil;
+}
